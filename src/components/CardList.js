ba475ac4dc702992b9a1aad3e5b9f49832995c15
Hernan de Soto <hdesoto@gmail.com>
2021-06-15 18:24:47 +0200

    Quitado error en CardList con ErrorBoundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,9 +3,9 @@ import Card from './Card';
 
 
 const CardList = ({ robots }) => {
-    if (true){
+   /*  if (true){
         throw new Error("noooooo!!");
-    }
+    } */
     return (
         <React.StrictMode>
         <div>
